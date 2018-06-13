62128e6b66c48a0f762b84c387fd6397e0c8fb2e
bipinkumar919 <bipinkumar919@gmail.com>
2018-06-13 21:20:45 +0530

    get serious dude

---
+++ DS/LinkedList/Misc/Set middle element as head.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node{
+	int data;
+	Node* next;
+};
+
+Node* head;
+
+void Insert(int data){
+	Node* node= new Node();
+	node->data=data;
+
+	if(head==NULL){
+		node->next=head;
+		head=node;
+		return;
+	}
+
+	Node* temp=head;
+
+	while(temp->next!=NULL)
+		temp=temp->next;
+
+	node->next= NULL;
+	temp->next=node;
+
+}
+
+void middleInsert(){
+	Node* node=head,*nnode=head,*prev=NULL;
+
+	while(nnode!=NULL && nnode->next!=NULL){
+		prev=node;
+		
+		nnode=nnode->next->next;
+		node=node->next;
+	}
+
+
+	prev->next=prev->next->next;
+	node->next=head;
+	head=node;
+
+	}
+
+void Print(){
+	Node* temp= head;
+	while(temp!=NULL){
+		cout<<temp->data<<"		";
+		temp=temp->next;
+	}
+}
+
+int main(){
+	head=NULL;
+
+	cout<<"Enter the no of elements:  ";
+	int m;
+	cin>>m;
+
+	for (int i = 0; i < m; ++i)
+	{
+		int a;
+		cout<<"Element: ";
+		cin>>a;
+		
+		Insert(a);
+	}
+
+	Print();
+
+	middleInsert();
+
+	Print();
+
+
+}
